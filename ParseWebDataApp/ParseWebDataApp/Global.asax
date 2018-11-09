b1b3567287ceecddc8137549f9f9f04620d1358c
chao1017 <chaoshuangjui@gmail.com>
2018-11-09 09:48:05 +0800

    add model and controller

---
+++ ParseWebDataApp/ParseWebDataApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ParseWebDataApp.WebApiApplication" Language="C#" %>
---
+++ ParseWebDataApp/ParseWebDataApp/Models/WebPageData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParseWebDataApp.Models
+{
+    /// <summary>
+    /// 解析商品資訊
+    /// </summary>
+    public class WebPageData
+    {
+        public string item_name { get; set; }//商品名稱
+        public int price { get; set; }//商品價格
+        public string pic { get; set; }//商品圖片
+    }
+}
